5fbf0e17e45ed619a8968c25809e2a8b1541a188
mtiderin <mtiderin@purdue.edu>
2025-03-30 12:57:21 -0400

    notes

--- linked_list.c
+++ linked_list.c
@@ -4,14 +4,19 @@
 
 struct list_node *new_node(size_t value) { return NULL; }
 
+//function to insert values to the head of the linked list
 void insert_at_head(struct linked_list *list, size_t value) {}
 
+//function to insert values to the tail of the linked list
 void insert_at_tail(struct linked_list *list, size_t value) {}
 
+//function to remove values from the head of the linked list
 size_t remove_from_head(struct linked_list *list) { return 0; }
 
+//function to remove values from the tail of the linked list
 size_t remove_from_tail(struct linked_list *list) { return 0; }
 
+//function to free linked lists, must implement myself
 void free_list(struct linked_list list) {}
 
 // Utility function to help you debugging, do not modify
--- queue.c
+++ queue.c
@@ -1,8 +1,11 @@
 #include "queue.h"
 #include "tile_game.h"
 
+// use list and serialize to queue
 void enqueue(struct queue *q, struct game_state state) {}
 
+//use list and deserialize to dequeue
 struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
 
+//use queue to implement a BFS to determine shortest number of moves
 int number_of_moves(struct game_state start) { return 0; }
